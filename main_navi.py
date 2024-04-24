10091873211e19aa35a89f51a6b2aa046353ff68
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-04-24 11:44:41 -0700

    completed dfs

--- main_navi.py
+++ main_navi.py
@@ -10,25 +10,38 @@
    Algorithm for pathfinding. Also incorporates GUI, utilizing a graph model.
 ********************************************************************************"""  
 from algorithms import bfs_search # Import external function BFS
+from algorithms import dfs_search # Import external function DFS
 
 # Main function for execution
 def main():
     # Placeholder locations (userinput later, will be included in Kiara's GUI) ------------------------- important!!!!
     start_location = "Library"
-    goal_location = "Computer Science Building"
+    goal_location = "Park"
     
     # Placeholder list (will need to rely on Robert's graph model) ---------------------------------------- important!!!!
     adjacency_list = {
-        "Library": ["Student Center", "Parking Lot"],
-        "Student Center": ["Library", "Cafeteria"],
-        "Cafeteria": ["Student Center", "Computer Science Building"],
-        "Computer Science Building": ["Cafeteria", "Parking Lot"],
-        "Parking Lot": ["Library", "Computer Science Building"]
+    "Library": ["Student Center", "Parking Lot", "Cafeteria"],
+    "Student Center": ["Library", "Cafeteria", "Theater"],
+    "Cafeteria": ["Student Center", "Library", "Gym", "Computer Science Building"],
+    "Gym": ["Cafeteria", "Theater"],
+    "Theater": ["Student Center", "Gym", "Coffee Shop"],
+    "Computer Science Building": ["Cafeteria", "Museum"],
+    "Parking Lot": ["Library", "Park"],
+    "Coffee Shop": ["Theater", "Park"],
+    "Museum": ["Computer Science Building", "Park"],
+    "Park": ["Coffee Shop", "Museum", "Library"]
     }
 
+    # Placeholder paths (once Kiara's GUI is completed, user input will select BFS, DFS or Dijkstra and choose a path) important!!!! 
     path = bfs_search(start_location, goal_location, adjacency_list) # use bfs on path
     if path: # placeholder prints (will be displayed in Kiara's GUI) ------------------------------------ important!!!!
-        print("Path found within", len(path)-1, "stops:", path) # print number of stops
+        print("BFS: Path found within", len(path)-1, "stops:", path) # print number of stops
+    else:
+        print("No path found.")
+
+    path2 = dfs_search(start_location, goal_location, adjacency_list) # use dfs on path
+    if path2: # placeholder prints (will be displayed in Kiara's GUI) ------------------------------------ important!!!!
+        print("DFS: Path found within", len(path2)-1, "stops:", path2) # print number of stops
     else:
         print("No path found.")
 
