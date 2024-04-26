38de7de013d388a0f31396be2c883de18651bc65
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-04-26 11:20:58 -0700

    fixed pathing

--- build/gui.py
+++ build/gui.py
@@ -9,7 +9,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\mxmoo\Downloads\Tkinter-Designer-master\Tkinter-Designer-master\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / "assets" / "frame0"
 
 
 def relative_to_assets(path: str) -> Path:
