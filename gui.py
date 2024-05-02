83ca2388cf84836a9870442d0397a8069952d488
Robert <freesnaz@gmail.com>
2024-05-02 08:18:58 -0700

    Edge paths
    Adjusted some python warnings nothing serious just refactoring and starting to create the paths so the graph is more fleshed out so more options for each path

--- gui.py
+++ gui.py
@@ -9,73 +9,77 @@
    This File: gui.py
    Description: Graphical output of pathfinding algorithims. Collects user input. 
 ********************************************************************************"""  
-from pathlib import Path # Create a path to the image file
-import networkx as nx # Used to create a graph and populate it with nodes
-from tkinter import Tk, Canvas, Button, PhotoImage, StringVar, OptionMenu, Label # Use necessary widgets for GUI
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg # Embed the Matplotlib figure in the Tkinter window
-from matplotlib.figure import Figure # Note: does not seem to be used yet -----------------------------------------------
-import matplotlib.pyplot as plt # Plot the graph on the Matplotlib figure
-import pandas as pd # Note: does not seem to be used yet ------------------------------------------------------------
-from data import * # Import all data from data.py
-from algorithms import bfs_search # Import external function BFS
-from algorithms import dfs_search # Import external function DFS
-
+from pathlib import Path  # Create a path to the image file
+import networkx as nx  # Used to create a graph and populate it with nodes
+from tkinter import Tk, Canvas, Button, PhotoImage, StringVar, OptionMenu, Label  # Use necessary widgets for GUI
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # Embed the Matplotlib figure in the Tkinter window
+from matplotlib.figure import Figure
+# Note: does not seem to be used yet -----------------------------------------------
+import matplotlib.pyplot as plt  # Plot the graph on the Matplotlib figure
+import pandas as pd  # Note: does not seem to be used yet ------------------------------------------------------------
+from data import *  # Import all data from data.py
+from algorithms import bfs_search  # Import external function BFS
+from algorithms import dfs_search  # Import external function DFS
 # Function to output the GUI
+
+
 def show_gui(adjacency_list):
     # Define path for assets
-    OUTPUT_PATH = Path(__file__).parent
-    ASSETS_PATH = OUTPUT_PATH / "assets" / "frame0"
-    def relative_to_assets(path: str) -> Path: 
-        return ASSETS_PATH / Path(path)
+    output_path = Path(__file__).parent
+    assets_path = output_path / "assets" / "frame0"
+
+    def relative_to_assets(path: str) -> Path:
+        return assets_path / Path(path)
 
     # Create Tkinter window
     window = Tk()
-    window.geometry("1400x1000") # ajusted window size
+    window.geometry("1400x1000")  # ajusted window size
     window.configure(bg="#FFFFFF")
-    G = nx.Graph() # create a NetworkX graph
+    g = nx.Graph()  # create a NetworkX graph
 
     # Add nodes to the graph
     for building in buildings:
-        G.add_node(building)
+        g.add_node(building)
 
     # Add coordinates to the nodes
     for building, coord in coordinates.items():
-        G.nodes[building]['pos'] = coord
+        g.nodes[building]['pos'] = coord
 
     # Populate the graph with edges and weights
     for building, adj_list in adjacency_list.items():
         for adj_building, weight in adj_list.items():
-            G.add_edge(building, adj_building, weight=weight)
+            g.add_edge(building, adj_building, weight=weight)
 
-    fig, ax = plt.subplots(figsize=(20, 11)) # create a Matplotlib figure and axis with adjusted size
-    background = plt.imread('assets/frame0/WEBSITEMAP.jpg') # load the background image
-    ax.imshow(background, extent=[0, 500, 0, 700]) # plot the image as background
+    fig, ax = plt.subplots(figsize=(20, 11))  # create a Matplotlib figure and axis with adjusted size
+    background = plt.imread('assets/frame0/WEBSITEMAP.jpg')  # load the background image
+    ax.imshow(background, extent=[0, 500, 0, 700])  # plot the image as background
 
     # Draw the graph on top of the image
-    pos = nx.get_node_attributes(G, 'pos')
-    nx.draw(G, pos, with_labels=False, node_size=300, node_color='#4258CA', font_size=8)
-    nx.draw_networkx_edges(G, pos, width=3, edge_color="#4258CA")
+    pos = nx.get_node_attributes(g, 'pos')
+    nx.draw(g, pos, with_labels=False, node_size=300, node_color='#4258CA', font_size=8)
+    nx.draw_networkx_edges(g, pos, width=3, edge_color="#4258CA")
 
     # Create a canvas widget to embed the Matplotlib figure
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.draw()
-    canvas.get_tk_widget().place(x=0, y=0) # adjusted placement to the far left
+    canvas.get_tk_widget().place(x=0, y=0)  # adjusted placement to the far left
 
     # Create a separate canvas for drawing shapes
     canvas_shapes = Canvas(
         window,
         bg="red",
-        height=100, # adjusts canvas height
+        height=100,  # adjusts canvas height
         width=1400,   
         bd=0,
         highlightthickness=0,
         relief="ridge"
     )
-    canvas_shapes.place(x=0, y=0) # set placement to the far left
+    canvas_shapes.place(x=0, y=0)  # set placement to the far left
 
     # Add rectangles using Canvas widget's create_rectangle method
     canvas_shapes.create_rectangle(0.0, 0.0, 1400.0, 100.0, fill="#FD8B21", outline="")
-    canvas_shapes.create_text(40.0, 9.0, anchor="nw", text="Smart Campus Navigation System", fill="#000000", font=("JosefinSansRoman Bold", 40))
+    canvas_shapes.create_text(40.0, 9.0, anchor="nw", text="Smart Campus Navigation System", fill="#000000",
+                              font=("JosefinSansRoman Bold", 40))
 
     # Create a pink canvas for general information
     canvas_general = Canvas(
@@ -87,23 +91,29 @@ def show_gui(adjacency_list):
         highlightthickness=0,
         relief="ridge"
     )
-    canvas_general.place(x=10, y=100) # set placement to the far left
+    canvas_general.place(x=10, y=100)  # set placement to the far left
 
     # Add rectangles around buttons
-    canvas_general.create_text(40.0, 10.0, anchor="nw", text="For accessible paths click the button below:", fill="black", font=("JosefinSansRoman Bold", 18))
-    canvas_general.create_text(40.0, 200.0, anchor="nw", text="Pick start and end points from the drop down menu:", fill="black", font=("JosefinSansRoman Bold", 18))
-    canvas_general.create_text(40.0, 350.0, anchor="nw", text="Choose preferred algorithm:", fill="black", font=("JosefinSansRoman Bold", 18))
+    canvas_general.create_text(40.0, 10.0, anchor="nw", text="For accessible paths click the button below:",
+                               fill="black",
+                               font=("JosefinSansRoman Bold", 18))
+    canvas_general.create_text(40.0, 200.0, anchor="nw", text="Pick start and end points from the drop down menu:",
+                               fill="black",
+                               font=("JosefinSansRoman Bold", 18))
+    canvas_general.create_text(40.0, 350.0, anchor="nw", text="Choose preferred algorithm:", fill="black",
+                               font=("JosefinSansRoman Bold", 18))
 
     # Accessibility Button
-    button_image_Access = PhotoImage(file=relative_to_assets("ButtonAccess.png"))
-    ButtonAccess = Button(
-        image=button_image_Access,
+    button_image_access = PhotoImage(file=relative_to_assets("ButtonAccess.png"))
+    button_access = Button(
+        image=button_image_access,
         borderwidth=0,
         highlightthickness=0,
-        command=lambda: print("Access clicked"), # Placeholder will need accessibility path later --------------------------------
+        command=lambda: print("Access clicked"),
+        # Placeholder will need accessibility path later --------------------------------
         relief="flat"
     )
-    ButtonAccess.place( # placement for the accessibility button
+    button_access.place(  # placement for the accessibility button
         x=100,
         y=160,
         width=472.0,
@@ -111,15 +121,15 @@ def show_gui(adjacency_list):
     )
 
     # BFS Button
-    button_image_BFS = PhotoImage(file=relative_to_assets("ButtonBFS.png"))
-    ButtonBFS = Button(
-        image=button_image_BFS,
+    button_image_bfs = PhotoImage(file=relative_to_assets("ButtonBFS.png"))
+    button_bfs = Button(
+        image=button_image_bfs,
         borderwidth=0,
         highlightthickness=0,
         command=lambda: display_result(canvas_result, bfs_search(start_building, end_building, adjacency_list), "BFS"),
         relief="flat"
     )
-    ButtonBFS.place(
+    button_bfs.place(
         x=455,
         y=500,
         width=200.0,
@@ -127,16 +137,16 @@ def show_gui(adjacency_list):
     )
 
     # DFS Button
-    button_image_DFS = PhotoImage(
+    button_image_dfs = PhotoImage(
         file=relative_to_assets("ButtonDFS.png"))
-    ButtonDFS = Button(
-        image=button_image_DFS,
+    button_dfs = Button(
+        image=button_image_dfs,
         borderwidth=0,
         highlightthickness=0,
         command=lambda: display_result(canvas_result, dfs_search(start_building, end_building, adjacency_list), "DFS"),
         relief="flat"
     )
-    ButtonDFS.place(
+    button_dfs.place(
         x=235,
         y=500,
         width=200.0,
@@ -144,23 +154,23 @@ def show_gui(adjacency_list):
     )
 
     # DA Button
-    button_image_DA = PhotoImage(
+    button_image_da = PhotoImage(
         file=relative_to_assets("ButtonDA.png"))
-    ButtonDA = Button(
-        image=button_image_DA,
+    button_da = Button(
+        image=button_image_da,
         borderwidth=0,
         highlightthickness=0,
         command=lambda: print("DA clicked"),
         relief="flat"
     )
-    ButtonDA.place(
+    button_da.place(
         x=15,
         y=500,
         width=200.0,
         height=120.0
     )
 
-    options = list(map(str, buildings)) # dropdown menu options
+    options = list(map(str, buildings))  # dropdown menu options
     # Datatype of menu text
     clicked1 = StringVar()
     clicked2 = StringVar()
@@ -192,17 +202,19 @@ def show_gui(adjacency_list):
     )
     canvas_result.place(x=10, y=650) 
 
-    window.resizable(False, False) # prevent window from being resized
-    window.mainloop() # keep running the GUI
+    window.resizable(False, False)  # prevent window from being resized
+    window.mainloop()  # keep running the GUI
 
-def display_result(canvas, path, algorithm_name): # expects both BFS and DFS paths
+
+def display_result(canvas, path, algorithm_name):  # expects both BFS and DFS paths
     if path:
-        formatted_path = '\n'.join(map(str, path)) # take out unwanted characters from string
-        result = f"{algorithm_name}: Path within {len(path) - 1} stops \n{formatted_path}" # print out # of stops and shows path
+        formatted_path = '\n'.join(map(str, path))  # take out unwanted characters from string
+        result = f"{algorithm_name}: Path within {len(path) - 1} stops \n{formatted_path}"
+        # print out # of stops and shows path
     else:
         result = f"{algorithm_name}: No path was found."
 
-    canvas.delete("result_text") # clear previous result text
+    canvas.delete("result_text")  # clear previous result text
 
     # Display the new result text
     canvas.create_text(
@@ -212,7 +224,5 @@ def display_result(canvas, path, algorithm_name): # expects both BFS and DFS pat
         text=result,
         fill="#000000",
         font=("Helvetica", 12),
-        tags="result_text" # tagged to allow manipulation later
+        tags="result_text"  # tagged to allow manipulation later
     )
-
-
--- main_navi.py
+++ main_navi.py
@@ -15,13 +15,24 @@ import gui # Call the GUI file
 def main():
     # Placeholder list (will need to rely on Robert's graph model) ---------------------------------------- important!!!!
     adjacency_list = {
-    "Parking A": {"Parking G": 5, "Titan Sport Complex": 3, "Parking & Transportation Office": 2, "A-South Parking": 7, "Children's Center": 8},
-    "Parking G": {"Parking A": 5, "Titan Sport Complex": 2, "Parking & Transportation Office": 9},
+    "Linda 01": {"Parking A to G 02" : 1,},
+    "Linda 02": {"Linda 01" : 1, "Parking G to Linda 01" : 1},
+    "StateLinda": {"State 01" : 1,},
+    "State 01": {"Parking A": 1, "StateLinda": 1,},
+    "Parking A": {"Parking A to G 01": 1, "State 01": 1,},
+    "Parking A to G 01": {"Parking A": 1, "Parking A to G 02": 1, "Titan Sporting Complex": 1},
+    "Parking A to G 02": {"Parking A to G 01": 1, "Parking G": 1},
+    "Parking G": {"Parking A to G 02": 1,  "Titan Sport Complex": 2, "Linda 01" :1},
+    "Parking G to Linda 01" :{"Parking G" : 1,},
+    "Titan Sport Complex": {"Parking A": 3, "Parking G": 2, "Arboretum": 4},
+
+
+
     "Arboretum": {"Housing Office": 3, "Residence Halls": 2, "Titan Sport Complex": 4, "Ruby Gerontology Center": 6, "Titan House": 2},
     "Children's Center": {"A-South Parking": 2, "Parking & Transportation Office": 6, "Parking A": 8},
     "Parking & Transportation Office": {"Parking A": 2, "Parking G": 9, "Children's Center": 6},
     "A-South Parking": {"Parking A": 7, "Children's Center": 2, "Parking & Transportation Office": 1},
-    "Titan Sport Complex": {"Parking A": 3, "Parking G": 2, "Arboretum": 4},
+
     "Housing Office": {"Arboretum": 3, "Residence Halls": 2, "Ruby Gerontology Center": 6},
     "Residence Halls": {"Arboretum": 2, "Housing Office": 2, "Ruby Gerontology Center": 7},
     "Corporation Yard": {"Titan House": 8, "University Police": 3},
