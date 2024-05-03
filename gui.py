e6f9795d28fb17b9439a87aa82af63b0793e5083
kguerra3411 <kguerra3411@csu.fullerton.edu>
2024-05-03 06:41:28 -0700

    graph change + access paths

--- gui.py
+++ gui.py
@@ -19,6 +19,8 @@ import pandas as pd  # Note: does not seem to be used yet ----------------------
 from data import *  # Import all data from data.py
 from algorithms import bfs_search  # Import external function BFS
 from algorithms import dfs_search  # Import external function DFS
+from tkinter import Tk, font
+
 # Function to output the GUI
 def show_gui(adjacency_list, alt_list):
     # Define path for assets
@@ -36,7 +38,7 @@ def show_gui(adjacency_list, alt_list):
 
     # Create Tkinter window
     window = Tk()
-    window.geometry("1400x1000")  # ajusted window size
+    window.geometry("1400x1000")  # adjusted window size
     window.configure(bg="#FFFFFF")
     g = nx.Graph()  # create a NetworkX graph
 
@@ -81,20 +83,20 @@ def show_gui(adjacency_list, alt_list):
 
     # Add rectangles using Canvas widget's create_rectangle method
     canvas_shapes.create_rectangle(0.0, 0.0, 1400.0, 100.0, fill="#FD8B21", outline="")
-    canvas_shapes.create_text(40.0, 9.0, anchor="nw", text="Smart Campus Navigation System", fill="#000000",
-                              font=("JosefinSansRoman Bold", 40))
+    canvas_shapes.create_text(40.0, 15.0, anchor="nw", text="Smart Campus Navigation System", fill="#000000",
+                              font=("Arial", 40))
 
     # Create a pink canvas for general information
     canvas_general = Canvas(
         window,
         bg="pink",
         height=600,  
-        width=650,
+        width=670,
         bd=0,
         highlightthickness=0,
         relief="ridge"
     )
-    canvas_general.place(x=10, y=100)  # set placement to the far left
+    canvas_general.place(x=0, y=100)  # set placement to the far left
 
     # Add rectangles around buttons
     canvas_general.create_text(40.0, 10.0, anchor="nw", text="For accessible paths click the button below:",
@@ -107,10 +109,11 @@ def show_gui(adjacency_list, alt_list):
                                font=("JosefinSansRoman Bold", 18))
     
     use_accessibility_adjacency_list = BooleanVar(value=True) # Bool to toggle reg and alt list
-    # Switch between regular and accessibility adjacency list --------------------------------------- sorry i need this here for me to find this function faster
+    # Switch between regular and accessibility adjacency list ---------------------------------------
     def toggle_adjacency_list():
         global adjacency_list, buildings, coordinates, options
         if use_accessibility_adjacency_list.get():
+            graph_to_alt()
             adjacency_list = alt_list  # make alt list the new list
             buildings = alt_buildings  # make alt buildings the new list
             coordinates = alt_coordinates  # make alt coordinates the new list
@@ -130,6 +133,7 @@ def show_gui(adjacency_list, alt_list):
                 drop1['menu'].add_command(label=option, command=lambda value=option: (clicked1.set(value), update_buildings()))
                 drop2['menu'].add_command(label=option, command=lambda value=option: (clicked2.set(value), update_buildings()))
         else:
+            graph_to_reg()
             adjacency_list = reg_list  # else restore the original
             buildings = reg_buildings
             coordinates = reg_coordinates
@@ -150,40 +154,56 @@ def show_gui(adjacency_list, alt_list):
 
             use_accessibility_adjacency_list.set(True) # toggle the button
         
-        #redraw_graph(ax)
+        #redraw_graph(ax)\
+    def clear_graph():
+        g.clear()
 
-    def redraw_graph(ax): #---------------------------------------------------- need this to redraw plz halp me!!!!!!!!!!
-        # Clear previous graph
-        # ax.clear() disabled for now
-        
+    def graph_to_reg():
+        clear_graph()
+        ax.clear()  # Clear the previous plot
+        g = nx.Graph()
+
             # Add nodes to the graph
-        for building in buildings:
-            g.add_node(building)
+        for building in reg_buildings:
+                g.add_node(building)
 
-        # Add coordinates to the nodes
-        for building, coord in coordinates.items():
-            g.nodes[building]['pos'] = coord
+            # Add coordinates to the nodes
+        for building, coord in reg_coordinates.items():
+                g.nodes[building]['pos'] = coord
 
-        # Populate the graph with edges and weights
+            # Populate the graph with edges and weights
         for building, adj_list in adjacency_list.items():
             for adj_building, weight in adj_list.items():
                 g.add_edge(building, adj_building, weight=weight)
 
-        fig, ax = plt.subplots(figsize=(20, 11))  # create a Matplotlib figure and axis with adjusted size
-        background = plt.imread('assets/frame0/WEBSITEMAP.jpg')  # load the background image
-        ax.imshow(background, extent=[0, 500, 0, 700])  # plot the image as background
-
-        # Draw the graph on top of the image
-        pos = nx.get_node_attributes(g, 'pos')
-        nx.draw(g, pos, with_labels=False, node_size=100, node_color='#4258CA', font_size=8)
-        nx.draw_networkx_edges(g, pos, width=3, edge_color="#000000")
-
-        # Create a canvas widget to embed the Matplotlib figure
-        canvas = FigureCanvasTkAgg(fig, master=window)
+        ax.imshow(background, extent=[0, 500, 0, 700])  # Redraw the background image
+        pos = nx.get_node_attributes(g, 'pos')  # Get node positions
+        nx.draw(g, pos, with_labels=False, node_size=100, node_color='#4258CA', font_size=8)  # Redraw the graph
+        nx.draw_networkx_edges(g, pos, width=3, edge_color="#000000")  # Redraw the edges
         canvas.draw()
-        canvas.get_tk_widget().place(x=0, y=0)  # adjusted placement to the far left
 
-        # Update the canvas
+    def graph_to_alt():
+        clear_graph()
+        ax.clear()  # Clear the previous plot
+        g = nx.Graph()
+
+            # Add nodes to the graph
+        for building in alt_buildings:
+            g.add_node(building)
+
+            # Add coordinates to the nodes
+        for building, coord in alt_coordinates.items():
+            g.nodes[building]['pos'] = coord
+
+            # Populate the graph with edges and weights
+        for building, adj_list in alt_list.items():
+            for adj_building, weight in adj_list.items():
+                g.add_edge(building, adj_building, weight=weight)
+
+        ax.imshow(background, extent=[0, 500, 0, 700])  # Redraw the background image
+        pos = nx.get_node_attributes(g, 'pos')  # Get node positions
+        nx.draw(g, pos, with_labels=False, node_size=100, node_color='#4258CA', font_size=8)  # Redraw the graph
+        nx.draw_networkx_edges(g, pos, width=3, edge_color="blue")  # Redraw the edges
         canvas.draw()
 
     # Accessibility Button
@@ -198,7 +218,7 @@ def show_gui(adjacency_list, alt_list):
     )
     button_access.place(  # placement for the accessibility button
         x=100,
-        y=160,
+        y=175,
         width=472.0,
         height=70.0
     )
@@ -215,8 +235,8 @@ def show_gui(adjacency_list, alt_list):
     button_bfs.place(
         x=455,
         y=500,
-        width=200.0,
-        height=120.0
+        width=190.0,
+        height=110.0
     )
 
     # DFS Button
@@ -232,8 +252,8 @@ def show_gui(adjacency_list, alt_list):
     button_dfs.place(
         x=235,
         y=500,
-        width=200.0,
-        height=120.0
+        width=190.0,
+        height=110.0
     )
 
     # DA Button
@@ -249,8 +269,8 @@ def show_gui(adjacency_list, alt_list):
     button_da.place(
         x=15,
         y=500,
-        width=200.0,
-        height=120.0
+        width=190.0,
+        height=110.0
     )
 
     options = list(map(str, buildings))  # dropdown menu options
@@ -269,21 +289,21 @@ def show_gui(adjacency_list, alt_list):
 
     # Create Dropdown menus
     drop1 = OptionMenu(window, clicked1, *options, command=update_buildings)
-    drop1.place(x=100, y=350) 
+    drop1.place(x=185, y=350) 
     drop2 = OptionMenu(window, clicked2, *options, command=update_buildings)
-    drop2.place(x=350, y=350) 
+    drop2.place(x=400, y=350) 
 
     # Create a separate canvas widget to display the result text
     canvas_result = Canvas(
         window,
         bg="pink",
         height=600,
-        width=650,
+        width=670,
         bd=0,
         highlightthickness=0,
         relief="ridge"
     )
-    canvas_result.place(x=10, y=650) 
+    canvas_result.place(x=0, y=650) 
 
     window.resizable(False, False)  # prevent window from being resized
     window.mainloop()  # keep running the GUI
